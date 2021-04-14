c73995940b69a8e1e758a34d7c38e2297c9f2d4a
frozenwhale <1614395133@qq.com>
2021-04-15 03:09:41 +0800

    finish lexical analysis

---
+++ lexical/README.md
@@ -0,0 +1,103 @@
+# 词法分析器
+
+使用python实现一个简单的C子集的编译器
+
+该程序使用python语言实现了C语言子集的词法分析程序。具体实现了
+
+- 识别给定的C语言子集的单词符号，并以(token, attribute)的形式输出。
+- 包含int（10进制，**8****进制，16进制**）,_+,-,*,/,=,==,>,<,<>,>=,<=,!,&&,||,**&,|**,if,**else,**while,get,put等**必做**和**可选项**。
+- 可以识别并跳过源程序的**（多个）**空白
+- 支持**预处理**，预处理将**跳过代码中的注释**（包括单行注释//和多行注释/**/），**可以选择进一步预删除部分字符（代码长度压缩**），包括删除多余的空格（连续空格只保留一个），删除换行符、制表符等。
+- 支持检查代码中的**所有**词法错误，出现错误时将报告错误所在的**代码行**（如果进行了预处理，则代码行号失去意义）以及在所有字符中的位置索引。出现错误时，将**跳过**错误所在的单词。
+- 可以统计代码的行数，字符数以及单词频数。
+
+## 错误处理说明
+
+针对状态机的每一个状态设置错误处理。具体的，对于边界符号而言，这里认为不存在错误（单符号）；对于其他的，若当前状态接收的字符不在允许的范围内（状态图上所示，以及边界符），则表示出错。例如变量名以数字开头或者非字母字符开头（9az）则认为错误；对于10进制整数，判断范围为0-9999，超过9999或者有小数点等均为错误；对于8进制数和16进制数也进行处理。特别的，十六进制以0x开头且可以允许前导0，如0x00000f。在存储时，将读取的8进制和16进制整数10进制形式存储。
+
+当遇到错误时，打印错误类型和所在代码行数以及字符位置，并跳过当前错误所在单词（以分界符为间隔）。
+
+## 实现
+
+程序使用Python实现，便于利用字典等数据结构，考虑到实验性质，**没有**利用python处理字符串的一些函数，如`replace`等批量替换删除，而是使用字符逐个扫描的方式。程序以面向对象的方式实现。
+
+- `init`，定义`other_char_list`包含换行符、制表符等，定义`border_char_list`表示边界符号，定义`reserved_words_list`表示保留字，定义`token_list`表示记号列表，`symbol_table`符号表，`attr_dict`表示符号属性翻译表。
+
+- 定义`print_line`函数，打印分割线，如：
+
+![img](https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/clip_image002.png)
+
+- 定义`filter`函数，进行文本预处理。将跳过代码注释（单行//和多行/**/），如果遇到注释不匹配，例如最后只有一个/，将抛出异常，告知错误终止程序。还可以选择将代码中的连续空格进行压缩，将换行符等删除。
+
+- 定义`insert_symbol`函数，进行符号表的插入，并维护符号表指针（通过建立符号表id字典实现）。
+
+- 定义`insert_token`函数，进行记号token的插入，使用元组(token, attribute)的结构。
+
+- 定义`print_error`函数，打印错误信息和错误发生的位置。格式为：
+
+  ```python
+  error {info} at line {error_line}, char at {error_pos }
+  ```
+
+  
+
+- 定义`scanner`函数，进行文本的扫描。整个扫描逻辑基于状态机图实现。
+
+  通过在开始识别新的单词符号之前，识别（多个）空格、换行符等并跳过，并记录行数。
+
+  由于python语言中没有switch-case语句，因此通过字典映射函数的方式实现状态机。定义子函数caseid，结构大体如下：
+
+  ```python
+  def caseid(token,c):
+  
+  if c == symbol_x:
+  
+    return state_id
+  
+  elif c in border_char_list:
+  
+    insert(token, token.attribute)
+  
+    return 0
+  
+  else:
+  
+    print_error(error_info)
+  
+    return -1
+  
+  
+  ```
+
+  
+
+  函数接收一个token记号串，以及下一个输入字符c，如果匹配，则进行状态转移到state_x；否则如果c是边界字符，则将token写入记号表；否则遇到错误打印错误信息，返回-1.
+
+  通过定义字典
+
+  ```python
+  switch={0:case0,1:case1,…,id:caseId}
+  ```
+
+  以字典函数调用的方式实现switch-case，返回值state分为0，>0以及-1.
+
+  ```python
+  state = switch [state](token, c)
+  ```
+
+  
+  - 0表示起点，需要清空token字符串并处理空格等信息
+  - \>0表示对应的状态
+  - -1表示错误，需要跳过单词（包括空格）等。
+
+- 定义`run`函数，依次进行文件加载，预处理`filter`，字符扫描`scanner`，并输出`token_list`. 
+
+- 定义`print_static_data`函数，打印统计信息（不包括注释）
+
+## 结果
+
+![image-20210415030544524](https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/image-20210415030544524.png)
+
+![image-20210415030558459](https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/image-20210415030558459.png)
+
+![image-20210415030608262](https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/image-20210415030608262.png)
---
+++ lexical/lexical.py
@@ -0,0 +1,405 @@
+
+
+class Lex():
+
+    def __init__(self):
+        self.other_char_list = ['\n', '\t']
+        self.border_char_list = ['+', '-', '*',
+                                 '(', ')', '/', '{', '}', ';', ',', ' ', '\n', '\t']
+        self.reserved_words_list = ['int', 'bool',
+                                    'while', 'if', 'else', 'put', 'get']
+
+        self.token_list = []
+        self.symbol_table = {}
+        self.symbol_table_id = {}
+        self.symbol_idx = 0
+        self.state = 0
+        self.attr_dict = {}
+        for e in self.border_char_list:
+            self.attr_dict[e] = '-'
+        for e in self.reserved_words_list:
+            self.attr_dict[e] = '-'
+        self.attr_dict['<'] = 'LT'
+        self.attr_dict['<='] = 'LE'
+        self.attr_dict['<>'] = 'NE'
+        self.attr_dict['>'] = 'GT'
+        self.attr_dict['>='] = 'GE'
+        self.attr_dict['!'] = 'NON'
+        self.attr_dict['&&'] = 'AND'
+        self.attr_dict['&'] = 'BAND'
+        self.attr_dict['||'] = 'OR'
+        self.attr_dict['|'] = 'BOR'
+        self.attr_dict['=='] = 'EQ'
+        self.attr_dict['='] = '-'
+
+    def print_line(self, info):
+        n = len(info)
+        nstar = (50-n)//2
+        print('*'*nstar, info, '*'*nstar)
+
+    def filter(self, text, delete_otherchar=False):
+        p, i = 0, 0
+        result = ''
+        text_len = len(text)
+        while True:
+            if i == text_len:
+                break
+            try:
+                if text[i] == '/' and text[i+1] == '/':
+                    while text[i] != '\n':
+                        i += 1
+                if text[i] == '/' and text[i+1] == '*':
+                    i += 2
+                    while text[i] != '*' and text[i+1] != '/':
+                        i += 1
+                    i += 2
+            except IndexError:
+                print('/ mismatch')
+                exit(0)
+
+            if delete_otherchar:
+                while i < text_len-1 and text[i] == ' ' and text[i+1] == ' ':
+                    i += 1
+                if text[i] not in self.other_char_list:
+                    result += text[i]
+                    p += 1
+            else:
+                result += text[i]
+                p += 1
+            i += 1
+        return result
+
+    def insert_symbol(self, symbol):
+        if symbol in self.symbol_table.keys():
+            return self.symbol_table[symbol]
+        self.symbol_table[symbol] = self.symbol_idx
+        self.symbol_table_id[self.symbol_idx] = symbol
+        self.symbol_idx += 1
+        return self.symbol_idx-1
+
+    def insert_token(self, token, attr):
+        if token == '':
+            return
+        if isinstance(attr, int):
+            self.token_list.append((token, attr))
+        else:
+            self.token_list.append((token, self.attr_dict[attr]))
+
+    def print_error(self, info):
+        print(f'{info} at line {self.cur_line}, char at {self.i}')
+
+    def scanner(self, s):
+
+        def case0(tk, c):
+            if c.isalpha():
+                return 1
+            elif c >= '1' and c <= '9':
+                return 2
+            elif c == '<':
+                return 5
+            elif c == '>':
+                return 6
+            elif c == '=':
+                return 7
+            elif c in self.border_char_list:
+                if tk == '!':
+                    self.insert_token('RELATION', tk)
+                    return 0
+                elif tk not in ['', ' ']:
+                    self.insert_token('LOGIC', tk)
+                    return 0
+                else:
+                    return 0
+
+            elif c == '&':
+                return 9
+            elif c == '|':
+                return 11
+            elif c == '0':
+                return 17
+            else:
+                self.print_error("invalid symbol "+c)
+                return -1
+
+        def case1(tk, c):
+            if c.isalnum():
+                return 1
+            else:
+                if c in self.border_char_list:
+                    if tk in self.reserved_words_list:
+                        self.insert_token(tk, '-')
+                        return 0
+                    else:
+                        idx = self.insert_symbol(tk)
+                        self.insert_token('ID', idx)
+                        return 0
+                else:
+                    self.print_error("invalid variable name")
+                    return -1
+
+        def case2(tk, c):
+            if c.isdigit():
+                return 3
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk))
+                return 0
+            else:
+                self.print_error('invalid number or variable name')
+                return -1
+
+        def case3(tk, c):
+            if c.isdigit():
+                return 4
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk))
+                return 0
+            else:
+                self.print_error("invalid 10 dec base number express")
+                return -1
+
+        def case4(tk, c):
+            if c.isdigit():
+                return 16
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk))
+                return 0
+            else:
+                self.print_error("invalid 10 dec base number express")
+                return -1
+
+        def case16(tk, c):
+            if c not in self.border_char_list:
+                self.print_error('invalid int number(should in range 0-9999)')
+                return -1
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk))
+                return 0
+            else:
+                self.print_error("invalid 10 dec base number express")
+                return -1
+
+        def case5(tk, c):
+            if c in ['=', '>']:
+                return 13
+            elif c in self.border_char_list:
+                self.insert_token('RELATION', tk)
+                return 0
+            else:
+
+                self.print_error("invalid relation operator")
+                return -1
+
+        def case6(tk, c):
+            if c == '=':
+                return 15
+            elif c in self.border_char_list:
+                self.insert_token('RELATION', tk)
+                return 0
+            else:
+                self.print_error("invalid relation operator <"+c)
+                return -1
+
+        def case7(tk, c):
+            if c == '=':
+                return 8
+            elif c in self.border_char_list:
+                self.insert_token('ASSIGN-OP', tk)
+                return 0
+            else:
+
+                self.print_error("invalid relation operator ="+c)
+                return -1
+
+        def case8(tk, c):
+            if c in self.border_char_list:
+                self.insert_token('RELATION', tk)
+                return 0
+            else:
+                self.print_error("invalid relation operator =="+c)
+                return -1
+
+        def case9(tk, c):
+            if c == '&':
+                return 10
+            elif c in self.border_char_list:
+                self.insert_token('LOGIC', tk)
+                return 0
+            else:
+                self.print_error("invalid logic operator &"+c)
+                return -1
+
+        def case10(tk, c):
+            if c in self.border_char_list:
+                self.insert_token('LOGIC', tk)
+                return 0
+            else:
+                self.print_error("invalid logic operator &"+c)
+                return -1
+
+        def case11(tk, c):
+            if c == '|':
+                return 12
+            elif c in self.border_char_list:
+                self.insert_token('LOGIC', tk)
+                return 0
+            else:
+                self.print_error("invalid logic operator |"+c)
+                return -1
+
+        def case12(tk, c):
+            if c in self.border_char_list:
+
+                self.insert_token('LOGIC', tk)
+                return 0
+            else:
+                self.print_error("invalid logic operator |"+c)
+                return -1
+
+        def case13(tk, c):
+            if c in self.border_char_list:
+                self.insert_token('RELATION', tk)
+                return 0
+            else:
+                self.print_error("invalid relation operator <"+c)
+                return -1
+
+        def case15(tk, c):
+            if c in self.border_char_list:
+                self.insert_token('RELATION', tk)
+                return 0
+            else:
+                self.print_error("invalid relation operator <=" + c)
+                return -1
+
+        def case17(tk, c):
+            if c == 'x':
+                return 19
+            elif c >= '1' and c <= '7':
+                return 18
+            elif c in self.border_char_list:
+                self.insert_token('NUM', 0)
+                return 0
+            else:
+                self.print_error('invalid number base expression')
+                return -1
+
+        def case18(tk, c):
+            if c >= '1' and c <= '7':
+                return 18
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk, 8))
+                return 0
+            else:
+                self.print_error('invalid 8 Oct base expression')
+                return -1
+
+        def case19(tk, c):
+            if c.isdigit() or c >= 'a' and c <= 'f':
+                return 20
+            else:
+                self.print_error('invalid 16 Hex base expression')
+                return -1
+
+        def case20(tk, c):
+            if c.isdigit() or c >= 'a' and c <= 'f':
+                return 20
+            elif c in self.border_char_list:
+                self.insert_token('NUM', int(tk, 16))
+                return 0
+            else:
+                self.print_error('invalid 16 Hex base expression')
+                return -1
+        switch = {
+            0: case0,
+            1: case1,
+            2: case2,
+            3: case3,
+            4: case4,
+            5: case5,
+            6: case6,
+            7: case7,
+            8: case8,
+            9: case9,
+            10: case10,
+            11: case11,
+            12: case12,
+            13: case13,
+            15: case15,
+            16: case16,
+            17: case17,
+            18: case18,
+            19: case19,
+            20: case20
+        }
+
+        self.i = 0
+        self.cur_line = 1
+        slen = len(s)
+
+        tk = ''
+        while self.i < slen:
+            c = s[self.i]
+            self.state = switch[self.state](tk, c)
+            tk += c
+            if self.state != 0:
+                self.i += 1
+                if self.state == -1:
+                    while s[self.i] not in ['\n', '\t', ' ']:
+                        if s[self.i] == '\n':
+                            self.cur_line += 1
+
+                        self.i += 1
+                    while s[self.i] in ['\n', '\t', ' ']:
+                        if s[self.i] == '\n':
+                            self.cur_line += 1
+                        self.i += 1
+                    self.state = 0
+                    tk = ''
+            else:
+                if c in self.border_char_list:
+                    self.i += 1
+                tk = ''
+                while self.i < slen and s[self.i] in ['\n', '\t', ' ']:
+                    if s[self.i] == '\n':
+                        self.cur_line += 1
+                    self.i += 1
+
+    def run(self, filename, preprocess=False):
+        file = open(filename, encoding='utf8')
+        text = file.read()
+        self.print_line('origin data')
+
+        print(text)
+
+        text = self.filter(text, delete_otherchar=preprocess)
+        self.print_line('pre processed data')
+
+        print(text)
+
+        print()
+        self.scanner(text)
+        self.print_line('lexical result')
+
+        for elem in self.token_list:
+            print(elem)
+
+    def print_static_data(self):
+        self.print_line('statictics info')
+        print('[range: without comments]')
+        print('total code lines', self.cur_line)
+        print('total characters counts', self.i)
+        from collections import Counter
+        counter = Counter(self.token_list)
+        for elem, cnt in counter.items():
+            if elem[0] == 'ID':
+                print(f'({self.symbol_table_id[elem[1]]}, {cnt})')
+            else:
+                print(f'({elem[0]}, {cnt})')
+
+
+if __name__ == '__main__':
+    # file = open("./test.cpp", encoding='utf8')
+    filepath = './test2.cpp'
+    lex = Lex()
+    lex.run(filepath, preprocess=False)
+    lex.print_static_data()
